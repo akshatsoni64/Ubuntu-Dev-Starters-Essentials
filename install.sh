da3ec521119d199db68ccd16ac23acb7922fe961
Akshat Soni <akshatsoni64@gmail.com>
2021-02-28 12:38:48 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,9 @@
-sudo apt install mysql-client mysql-common mysql-server nginx nodejs npm filezilla openvpn gnome-tweaks kazam git sqlite3 sqlitebrowser net-tools unixodbc unixodbc-dev build-essentials checkinstall libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
-sudo snap install vlc shutter code spotify atom discord postman skype
+sudo apt install mysql-client mysql-common mysql-server nginx nodejs npm filezilla openvpn gnome-tweaks kazam git sqlite3 sqlitebrowser net-tools unixodbc unixodbc-dev build-essential checkinstall libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+sudo snap install vlc
+sudo snap install shutter
+sudo snap install code --classic
+sudo snap install spotify
+sudo snap install atom --classic
+sudo snap install discord
+sudo snap install postman
+sudo snap install skype
