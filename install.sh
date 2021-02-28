ba33e8fae03a96e93e6f01086cd78a72e9616921
Akshat Soni <akshatsoni64@gmail.com>
2021-02-28 12:33:45 +0530

    Script added

---
+++ install.sh
@@ -0,0 +1,26 @@
+sudo apt install mysql-client
+sudo apt install mysql-common
+sudo apt install mysql-server
+sudo apt install nginx
+sudo apt install nodejs
+sudo apt install npm
+sudo apt install filezilla
+sudo apt install openvpn
+sudo apt install gnome-tweaks
+sudo apt install kazam
+sudo apt install git
+sudo apt install sqlite3
+sudo apt install sqlitebrowser
+sudo apt install net-tools
+sudo apt install unixodbc
+sudo apt install unixodbc-dev
+
+
+sudo snap install vlc
+sudo snap install shutter
+sudo snap install code
+sudo snap install spotify
+sudo snap install atom
+sudo snap install discord
+sudo snap install postman
+sudo snap install skype
